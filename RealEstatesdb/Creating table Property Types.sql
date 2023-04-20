b6587cabd420130ad7f89986bf43eea00a72c183
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2023-04-20 16:37:24 +0300

    Delete RealEstatesdb directory

--- RealEstatesdb/Creating table Property Types.sql
+++
@@ -1,7 +0,0 @@
-﻿use RealEstatesdb;
-
---Създаване на таблица за типовете Имоти
-CREATE TABLE property_types(
-	property_type_id INT PRIMARY KEY,
-	property_type_name VARCHAR(50)
-);
--- RealEstatesdb/Creating table comment_statuuuses.sql
+++
@@ -1,7 +0,0 @@
-﻿use RealEstatesdb;
-
---Създаване на таблица за статус на коментарите
-CREATE TABLE comment_statuses(
-	comment_status_id TINYINT PRIMARY KEY,
-	comment_status VARCHAR(50)
-);
